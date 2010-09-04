7cd821e400e9499beef67240da45573b93e32989
Dan Larkin <dan@danlarkin.org>
2010-09-04 19:26:45 -0400

    use datalog to store all runtime data, instead of a series of refs

--- src/subrosa/server.clj
+++ src/subrosa/server.clj
@@ -1,24 +1,28 @@
 (ns subrosa.server
+  (:use [clojure.contrib.datalog.database :only [make-database]])
   (:import [java.util Date]
            [java.net InetAddress]))
+
+(defn make-subrosa-db []
+  (make-database
+   (relation :user [:nick :real-name :user-name :channel :pending?])
+   (index :user :nick)
+   (index :user :channel)
+
+   (relation :room [:name :topic])
+   (index :room :name)
+
+   (relation :user-in-room [:user-nick :room-name])
+   (index :user-in-room :user-nick)
+   (index :user-in-room :room-name)))
+
+(defonce db (ref (make-subrosa-db)))
 
 (defonce +server+ {:host (.getHostName (InetAddress/getLocalHost))
                    :version "subrosa-1.0.0-SNAPSHOT"
                    :started (Date.)})
-(defonce +pending-connections+ (ref {}))  ; channel -> successful commands set
-(defonce +channels+ (ref {}))             ; channel -> nick
-(defonce +nicks+ (ref {}))                ; nick    -> user info map
-;;                                                     {:user-name ""
-;;                                                      :real-name ""}
-(defonce +rooms+ (ref {}))                ; room    -> room info map
-;;                                                     {:nicks #{}
-;;                                                      :topic ""
-;;                                                     }
 
 (defn reset-all-state! []
   (dosync
    (alter-var-root #'+server+ assoc :started (Date.))
-   (ref-set +pending-connections+ {})
-   (ref-set +channels+ {})
-   (ref-set +nicks+ {})
-   (ref-set +rooms+ {})))
+   (ref-set db (make-subrosa-db))))
