6e95ebf3104cbfa82ccd5fd78fa3eaf3bad76409
Dan Larkin <dan@danlarkin.org>
2010-10-16 17:46:01 -0400

    add lein-release plugin

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.jboss.netty/netty "3.2.1.Final"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [lein-release "1.1.1"]])
