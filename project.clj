b9b32954645242542716f53ad9008cfd70f082cf
Dan Larkin <dan@danlarkin.org>
2010-10-24 15:41:26 -0400

    ok... FIX jboss repo

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject subrosa "1.0.0-SNAPSHOT"
   :main subrosa.main
-  :repositories [{"jboss"
-                  "http://repository.jboss.org/nexus/content/groups/public/"}]
+  :repositories {"jboss"
+                 "http://repository.jboss.org/nexus/content/groups/public/"}
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.jboss.netty/netty "3.2.1.Final"]]
