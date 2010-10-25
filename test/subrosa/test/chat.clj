738856c0552f2f573b12688561c08a899f0f50c6
Dan Larkin <dan@danlarkin.org>
2010-10-25 12:02:07 -0400

    fix PRIVMSG tests to hopefully no longer fail intermittently

--- test/subrosa/test/chat.clj
+++ test/subrosa/test/chat.clj
@@ -9,21 +9,25 @@
     (transmit s1 "NICK dan")
     (transmit s1 "USER dan 0 * :Dan Larkin")
     (transmit s1 "JOIN #foo")
+    (is (received? s1 #"JOIN #foo"))
     (with-connection s2
       (transmit s2 "NICK dan2")
       (transmit s2 "USER dan 0 * :Dan Larkin")
       (transmit s2 "JOIN #foo")
-      (transmit s2 "PRIVMSG #foo :Hello, World!"))
+      (transmit s2 "PRIVMSG #foo :Hello, World!")
+      (Thread/sleep 1000)) ; give some time for the message to get to the server
     (is (received? s1 #"dan2!dan@.* PRIVMSG #foo :Hello, World!"))))
 
 (deftest basic-privmsg-to-user
   (with-connection s1
     (transmit s1 "NICK dan")
     (transmit s1 "USER dan 0 * :Dan Larkin")
+    (is (received? s1 #"Welcome to the .* dan$"))
     (with-connection s2
       (transmit s2 "NICK dan2")
       (transmit s2 "USER dan 0 * :Dan Larkin")
-      (transmit s2 "PRIVMSG dan :Hello, Dan!"))
+      (transmit s2 "PRIVMSG dan :Hello, Dan!")
+      (Thread/sleep 1000)) ; give some time for the message to get to the server
     (is (received? s1 #"dan2!dan@.* PRIVMSG dan :Hello, Dan!"))))
 
 (deftest privmsg-error-cases
