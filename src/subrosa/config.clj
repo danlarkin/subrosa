4b0ad0362b8648bddfae058045496a63d426d82d
Dan Larkin <dan@danlarkin.org>
2010-11-23 22:48:18 -0500

    add a config-override function which can be used to override configs for tests

--- src/subrosa/config.clj
+++ src/subrosa/config.clj
@@ -1,6 +1,15 @@
 (ns subrosa.config
-  (:use [subrosa.utils :only [load-resource]]))
+  (:use [subrosa.utils :only [merge-nested load-resource]]))
+
+(defn get-config-map []
+  (when-let [url (load-resource "subrosa.clj")]
+    (read-string (slurp url))))
 
 (defn config [& ks]
-  (when-let [url (load-resource "subrosa.clj")]
-    (reduce get (read-string (slurp url)) ks)))
+  (reduce get (get-config-map) ks))
+
+(defn config-override
+  "Bind over subrosa.config/config to put custom config in tests."
+  [override-map]
+  (fn [& ks]
+    (reduce get (merge-nested (get-config-map) override-map) ks)))
--- src/subrosa/utils.clj
+++ src/subrosa/utils.clj
@@ -7,6 +7,11 @@
        (cons (first s1) (cons (first s2)
                               (interleave-all (rest s1) (rest s2))))))))
 
+(defn merge-nested [v1 v2]
+  (if (and (map? v1) (map? v2))
+    (merge-with merge-nested v1 v2)
+    v2))
+
 (defn load-resource [resource-name]
   (-> (Thread/currentThread)
       (.getContextClassLoader)
