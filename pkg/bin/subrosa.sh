799bf7219c5fe9c56fb75e8fb884f709b3da2344
Dan Larkin <dan@danlarkin.org>
2010-11-27 14:49:22 -0500

    include etc/ in the tarball produced by `lein release`

--- pkg/bin/subrosa.sh
+++ pkg/bin/subrosa.sh
@@ -6,4 +6,4 @@ JAVA_CMD=${JAVA_CMD:-"java"}
 JAVA_OPTS=${JAVA_OPTS:-"-server"}
 SUBROSA_PORT=${SUBROSA_PORT:-"6667"}
 
-exec $JAVA_CMD $JAVA_OPTS -cp "$BASEDIR/lib/*" subrosa.main $SUBROSA_PORT
+exec $JAVA_CMD $JAVA_OPTS -cp "$BASEDIR/etc/:$BASEDIR/lib/*" subrosa.main $SUBROSA_PORT
---
+++ pkg/etc/subrosa.clj
@@ -0,0 +1,1 @@
+../../etc/subrosa.clj
