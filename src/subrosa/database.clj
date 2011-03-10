8ea98f6fefb8b049249e368e2fe23e333129ede7
Dan Larkin <dan@danlarkin.org>
2011-03-09 20:46:17 -0500

    database get/delete/put should be thin wrappers over pure functions

--- src/subrosa/database.clj
+++ src/subrosa/database.clj
@@ -9,25 +9,39 @@
     m
     (assoc m :id (str (UUID/randomUUID)))))
 
+(defn get*
+  ([db table]
+     (vals (get-in db [table :id])))
+  ([db table column value]
+     (get-in db [table column value])))
+
 (defn get
   ([table]
-     (vals (get-in @db [table :id])))
+     (get* @db table))
   ([table column value]
-     (get-in @db [table column value])))
+     (get* @db table column value)))
+
+(defn delete* [db table m]
+  (assoc db table (reduce (fn [a [k v]]
+                            (update-in a [k] dissoc v))
+                          (db table)
+                          m)))
 
 (defn delete [table id]
   (dosync
    (let [m (get table :id id)]
-     (alter db assoc table (reduce (fn [a [k v]]
-                                     (update-in a [k] dissoc v))
-                                   (@db table)
-                                   m)))))
+     (alter db delete* table m))))
+
+(defn put* [db table m]
+  (assoc db table (reduce (fn [a [k v]]
+                            (update-in a [k] assoc v m))
+                          (db table)
+                          m)))
 
 (defn put [table m]
   (dosync
    (let [m (ensure-id m)]
-     (delete table (:id m))
-     (alter db assoc table (reduce (fn [a [k v]]
-                                     (update-in a [k] assoc v m))
-                                   (@db table)
-                                   m)))))
+     (alter db (fn [db]
+                 (-> db
+                     (delete* table m)
+                     (put* table m)))))))
