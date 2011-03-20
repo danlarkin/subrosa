23aedc028066bed71bc6d6c04688314e9a67e8ee
Dan Larkin <dan@danlarkin.org>
2011-03-19 22:18:13 -0400

    refactor subrosa.hooks to allow for binding over the current hooks

--- src/subrosa/hooks.clj
+++ src/subrosa/hooks.clj
@@ -1,18 +1,26 @@
 (ns subrosa.hooks)
 
-(defonce get-hook
-  (comp
-   (memoize
-    (fn [hook] (atom {})))
-   (memfn toLowerCase)
-   str))
+(defonce hooks (ref {}))
+
+(def format-hook
+  (comp (memfn toLowerCase) str))
+
+(defn get-hook [hook]
+  (let [hook (format-hook hook)]
+    (dosync
+     (if-let [hook-map (@hooks hook)]
+       hook-map
+       (do
+         (alter hooks assoc hook {})
+         {})))))
 
 (defn add-hook [tag hook fn]
-  (swap! (get-hook hook) assoc tag fn))
+  (dosync
+   (alter hooks update-in [(format-hook hook)] assoc tag fn)))
 
 (defn hooked? [hook]
-  (not (empty? @(get-hook hook))))
+  (not (empty? (get-hook hook))))
 
 (defn run-hook [hook & args]
-  (doseq [[tag listener] @(get-hook hook)]
+  (doseq [[tag listener] (get-hook hook)]
     (apply listener hook args)))
