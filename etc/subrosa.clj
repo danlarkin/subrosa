e2b2762d32f1aca7ac2c01bb4773a7214b384355
Lee Hinman <lee@writequit.org>
2011-05-10 08:49:40 -0600

    add catchup config options

--- etc/subrosa.clj
+++ etc/subrosa.clj
@@ -1,5 +1,9 @@
 {:host "localhost"
  :network "Subrosa"
+ :catchup {:max-msgs-per-room 100
+           :default-catchup-size 40
+           :date-format "MM-dd"
+           :time-format "HH:mm:ss"}
  :ssl {:keystore nil
        :password nil}
  :password nil
