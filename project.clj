d454ade078d6baa5d54a2b1f7c4480a68f6e7433
Brian McDevitt <brian.p.mcdevitt@gmail.com>
2012-11-29 14:20:04 -0600

    Migration to Leiningen 2.0.0.

--- project.clj
+++ project.clj
@@ -1,8 +1,4 @@
-(defproject subrosa (.trim (slurp "etc/version.txt"))
-  :main subrosa.main
-  :resources-path "etc"
-  :repositories {"jboss"
-                 "http://repository.jboss.org/nexus/content/groups/public/"}
+(defproject subrosa/subrosa (.trim (slurp "etc/version.txt")) 
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.4"]
                  [org.clojure/tools.namespace "0.1.3"]
@@ -10,5 +6,10 @@
                  [log4j "1.2.16"]
                  [org.jboss.netty/netty "3.2.1.Final"]
                  [swank-clojure "1.3.2"]]
-  :dev-dependencies [[lein-tar "1.0.5"]
-                     [commons-io/commons-io "2.2"]])
+  :profiles {:dev {:dependencies [[commons-io/commons-io "2.2"]]}}
+  :repositories {"jboss"
+                 "http://repository.jboss.org/nexus/content/groups/public/"}
+  :resource-paths ["etc"]
+  :main subrosa.main
+  :min-lein-version "2.0.0"
+  :plugins [[lein-tar "1.1.0"]])
